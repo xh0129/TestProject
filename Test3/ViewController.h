4162d16356a7c1771a7c0d3d68c08757b95b5c7e
Lee <823974987@qq.com>
2016-09-26 07:43:45 +0800

    xiaoheng123
    haha

---
+++ Test3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test3
+//
+//  Created by liguangjun on 16/9/25.
+//  Copyright © 2016年 liguangjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test3/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Test3
+//
+//  Created by liguangjun on 16/9/25.
+//  Copyright © 2016年 liguangjun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"haha");
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
